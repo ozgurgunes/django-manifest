b3013d84149cd17e00366ad03309711a70830e16
Ozgur <ozgur@Ozgurs-iMac.local>
2012-05-09 02:29:39 +0300

    navigation_tag

--- manifest/core/templatetags/navigation.py
+++ manifest/core/templatetags/navigation.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
+import re
 from django import template
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
 @register.simple_tag
-def active(request, pattern):
-    import re
+def active_url(request, urls):
+    if request.path in (reverse(url) for url in urls.split()):
+        return 'active'
+    return ''
+
+@register.simple_tag
+def active_path(request, pattern):
     if re.search(pattern, request.path):
         return 'active' 
     return ''
+  
+    
